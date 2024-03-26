d75e341bbe1f5be69949d56c8b75c3d32124b18e
Shivakumar H M <shivakumar.hm@incture.com>
2024-03-26 10:33:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+            .then(response => response.json())
+            .then(data => {
+                setData(data);
+                console.log(data);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'title', headerName: 'Title', width: 200 },
+        { field: 'description', headerName: 'Description', width: 300 },
+        { field: 'rating', headerName: 'Rating', width: 130 },
+        { field: 'stock', headerName: 'Stock', width: 130 },
+    ];
+
+    return (
+        <div style={{ height: 500, width: '100%' }}>
+            <h1>Hello</h1>
+            {data && (
+                <DataGrid
+                    rows={data.products}
+                    columns={columns}
+                    checkboxSelection
+                />
+                )} 
+
+
+
+                {/* // <TableContainer >
+                //     <Table>
+                //         <TableHead>
+                //             <TableRow>
+                //                 <TableCell>ID</TableCell>
+                //                 <TableCell>Title</TableCell>
+                //                 <TableCell>Description</TableCell>
+                //                 <TableCell>Rating</TableCell>
+                //                 <TableCell>Stock</TableCell>
+                //             </TableRow>
+                //         </TableHead>
+                //         <TableBody>
+                //             {data.products.map((row) => ( */}
+                {/* //                 <TableRow key={row.id}>
+                //                     <TableCell>{row.id}</TableCell>
+                //                     <TableCell>{row.title}</TableCell>
+                //                     <TableCell>{row.description}</TableCell>
+                //                     <TableCell>{row.rating}</TableCell>
+                //                     <TableCell>{row.stock}</TableCell>
+                //                 </TableRow>
+                //             ))}
+                //         </TableBody> */}
+                {/* //     </Table> */}
+                {/* // </TableContainer>
+            )} */}
+        </div>
+    );
 }
+
 
 export default App;
